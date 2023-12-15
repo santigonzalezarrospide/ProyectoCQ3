994bfdbdaf8a1a6e5408e13667379b0d4344af68
santigonzalezarrospide <santiago.g.a16@gmail.com>
2023-12-15 16:41:02 -0300

    inicio proyecto cq3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './core/layout/footer/footer.component';
+import { LandingPageComponent } from './core/layout/landing-page/landing-page.component';
+import { NavBarComponent } from './core/layout/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    LandingPageComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
